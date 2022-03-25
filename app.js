c2f135931ec982c1be5499db3115eabbdfc252aa
ro <15965394977@163.com>
2022-03-25 14:54:33 +0800

    cursor Manage

--- app.js
+++ app.js
@@ -1,36 +1,45 @@
 #! /usr/bin/env node
 import {center,right}from './lib/text.js'
+import {cursorManege}from './lib/manage.js'
 import * as fs from 'fs'
 var {stdin,stdout} =process
 // const clog = console.log
+
 const clog = str=> process.stdout.write(str)
 const clear = console.clear
 stdin.setRawMode(true)
 stdin.resume()
 stdin.setEncoding('utf8')
 
-const storage = 'storage.txt';
-// fs.writeFile('storage.txt','- rewrite ansi\n',err=>err)
-//TODO Rewrite ansi()
-let position=[0,0]
+
 let page=reset([]);
-page.fill('#'.repeat(width(-2)));
-stdout.cursorTo(...position)
+page.fill('='.repeat(width(-2)));
+
+const storage = 'storage.txt';
 fs.readFile(storage,(err,data)=>{
 	let lines=data.toString().trim().split('\n')
 	page.splice(0,lines.length,...lines)
 })
 
 page.forEach((item,index,arr)=>{
+	// add line number
 	arr[index]=index.toString().padStart(2," ")+arr[index]
 })
 
+let cursor = cursorManege();
+cursor.xBound=(x)=>clamp(x,0,width(-1));
+cursor.yBound=(y)=>clamp(y,0,height(-1));
+cursor.afterMove=()=>{
+	stdout.cursorTo(...cursor.value())
+}
+
 TUI()
+cursor.move([0,0])
 
 function TUI(key='Nokey'){
 
 	page[height(-2)]='>:_'.green().grey()
-	page[height(-1)]=`width:${width()}, height:${height()}, key:${key.charCodeAt(0)}-${key}`.green()
+	page[height(-1)]=`width:${width()}, height:${height()}, key:${key.charCodeAt(0)}-${key} ${cursor.value()}`.green()
 	// let show=page.map((str)=> str.slice(0,width()) )
 	//it will slice my ANSI
 	clog(page.join('\n'))
@@ -44,11 +53,9 @@ stdin.on('data',function(key){
 	else if (key==='\x01'){
 		clog("trl+A")
 	}
-	let dir=getDirection(key)
-	position[0]+=dir[0]
-	position[1]+=dir[1]
+
 	TUI(key)
-	stdout.cursorTo(...position);
+	cursor.move(getDirection(key))
 })
 
 stdout.on('resize',()=>{
@@ -57,6 +64,7 @@ stdout.on('resize',()=>{
 	TUI()
 	//clog('resize: '+stdout.getWindowSize().toString())
 })
+
 
 function getDirection(key){
 	let x=0,y=0;
@@ -76,4 +84,9 @@ function height(offset=0){
 }
 function width(offset=0){
 	return process.stdout.getWindowSize()[0]+offset
+}
+function clamp(x,min,max){
+	if (x<min)x=min;
+	if (x>max)x=max;
+	return x
 }
